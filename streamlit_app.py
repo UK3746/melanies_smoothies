1d877a5d5bae14435fb9c281152c510ca6395790
UK3746 <utkarshakamble3746@gmail.com>
2024-09-08 12:49:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,5 +57,5 @@ except Exception as e:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
