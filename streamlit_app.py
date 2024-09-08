9c2b29a08e00af155387f55fe02b0b1570b517e9
UK3746 <utkarshakamble3746@gmail.com>
2024-09-08 15:14:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 try:
     # Fetch data from Snowflake
-    my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON")).to_pandas()
+    my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
     st.my_dataframe(data=my_dataframe, use_container_width=True)
 
     pd_df = my_dataframe.to_pandas()
