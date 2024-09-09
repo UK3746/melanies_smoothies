00a2f6363f640589f350ed4a9d7a27861c3628d4
UK3746 <utkarshakamble3746@gmail.com>
2024-09-09 15:17:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
     
         
-    st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
+st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
